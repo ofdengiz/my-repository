06f570744a3b992472af9c94d7a9895a8e365add
Omer Faruk Dengiz <omerdengiz368@gmail.com>
2023-01-23 22:02:30 -0500

    made some changes in app.py

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -16,16 +16,16 @@ def main_post():
     if request.method == 'POST':
         alpha = str(request.form['number'])        
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Altaz', not_valid=True)
+            return render_template('index.html', developer_name='Omer', not_valid=True)
         else:
             number = int(alpha)
             if not 0 < number < 4000:
-                return render_template('index.html', developer_name='Altaz', not_valid=True)
+                return render_template('index.html', developer_name='Omer', not_valid=True)
             else:
                 roman_number = convert(number)
-                return render_template('result.html', number_decimal=number, number_roman=roman_number, developer_name='Altaz')
+                return render_template('result.html', number_decimal=number, number_roman=roman_number, developer_name='Omer')
     else:
-        return render_template('index.html', developer_name='Altaz', not_valid=False)
+        return render_template('index.html', developer_name='Omer', not_valid=False)
 
 if __name__ == '__main__':
     #app.run(debug=True)
